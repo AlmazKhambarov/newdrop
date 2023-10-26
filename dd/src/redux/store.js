ce4af26bbe279cbfd76e95eb6f5c6c9dd04b465e
Wp <wp@xsw.local>
2023-10-26 19:08:42 +0500

    change

--- dd/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import filesSlice from "./filesSlice/filesSlice";
-
-const store = configureStore({
-    reducer:{
-        files:filesSlice,
-    }
-})
-export default store;
